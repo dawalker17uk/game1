ae444f683985349b44437e341555684f18328ff4
David Walker <dawalker17uk@hotmail.com>
2017-12-06 08:55:31 +0000

    Loggin player view point using grabber

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Grabber.h"
+#include "Engine/World.h"
 
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -20,7 +22,7 @@ void UGrabber::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!") );
+	//UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!") );
 }
 
 
@@ -29,6 +31,20 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get player view point this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotator;
+
+	if (GetWorld())
+	{
+		GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotator
+		);
+
+		UE_LOG(LogTemp, Warning, TEXT("Location: %s , Rotator: %s"), 
+			*PlayerViewPointLocation.ToString(), 
+			*PlayerViewPointRotator.ToString())
+	}
 }
 
